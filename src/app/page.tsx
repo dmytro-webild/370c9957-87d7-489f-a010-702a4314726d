ed0bea1dc59015c5b10e42845cb9fbc8f29a027a
bender <kudin.dima@gmail.com>
2026-05-04 20:32:38 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import FeatureCardTen from '@/components/sections/feature/FeatureCardTen';
 import FooterCard from '@/components/sections/footer/FooterCard';
 import HeroSplitDualMedia from '@/components/sections/hero/HeroSplitDualMedia';
 import NavbarStyleFullscreen from '@/components/navbar/NavbarStyleFullscreen/NavbarStyleFullscreen';
-import ProductCardThree from '@/components/ecommerce/productCatalog/ProductCatalogItem';
 import ProductCardOne from '@/components/sections/product/ProductCardOne';
 import TestimonialCardOne from '@/components/sections/testimonial/TestimonialCardOne';
 import TextAbout from '@/components/sections/about/TextAbout';
@@ -37,31 +36,31 @@ export default function LandingPage() {
               { name: "Products", id: "products" },
               { name: "Contact", id: "contact" },
             ]}
-            brandName="Senerita+ Libya"
+            brandName="Senerita Libya"
           />
         </div>
 
         <div id="hero" data-section="hero">
           <HeroSplitDualMedia
             background={{ variant: "radial-gradient" }}
-            title="Senerita+Libya منتجات طبيعية خالية من السلفات والبرابين"
-            description="مستخلصات طبيعية تناسب جميع انواع الشعر "
-            tag="عنايتك بشعرك تبداء من هنا "
+            title="Senerita Libya منتجات طبيعية خالية من السلفات والبارابين"
+            description="مستخلصات طبيعية تناسب جميع أنواع الشعر"
+            tag="عنايتك بشعرك تبدأ من هنا"
             mediaItems={[
               { videoSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Cx9nk9sCSbinRaOXqM7TyaxmbU/uploaded-1777754874673-n0so01y6.mp4", imageAlt: "Senerita hair care collection" },
               { videoSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Cx9nk9sCSbinRaOXqM7TyaxmbU/uploaded-1777741140013-fiujdr77.mp4", imageAlt: "Senerita product line up" }
             ]}
             mediaAnimation="slide-up"
             rating={5}
-            ratingText="منتجات آمنة %100"
+            ratingText="منتجات آمنة 100%"
           />
         </div>
 
         <div id="about" data-section="about">
           <TextAbout
             useInvertedBackground={false}
-            tag="كوني متاكدة من اختيارك الصحيح لشعرك "
-            title="مستخلصات طبيعية مصممة لتناسب جميع انواع الشعر  "
+            tag="كوني متأكدة من اختيارك الصحيح لشعرك"
+            title="مستخلصات طبيعية مصممة لتناسب جميع أنواع الشعر"
           />
         </div>
 
@@ -72,12 +71,12 @@ export default function LandingPage() {
             gridVariant="three-columns-all-equal-width"
             useInvertedBackground={true}
             products={[
-              { id: "p1", name: "فوائد شامبو وسيروم الكافيين ", price: " غني بالكافيين والكولاجين والبيوتين لمعالجة تساقط الشعر وتغذية الشعر وتعزيز بصيلاتها من الجذور وجعلها أقوى وألمع مناسب للاستخدام اليومي لشعر ناعم وطبيعي مستحضر لجميع أنواع الشعر خالي من السلفات و البرابين ", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Cx9nk9sCSbinRaOXqM7TyaxmbU/uploaded-1777741219362-l83s1kf5.jpg" },
-              { id: "p2", name: "شامبو+ سيروم الكافين + شامبو بذور العنب + شامبو إكليل الجبل", price: " غني بالكافيين والكولاجين والبيوتين لمعالجة تساقط الشعر وتغذية الشعر وتعزيز بصيلاتها من الجذور وجعلها أقوى وألمع مناسب للاستخدام اليومي لشعر ناعم وطبيعي مستحضر لجميع أنواع الشعر خالي من السلفات و البرابين ", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Cx9nk9sCSbinRaOXqM7TyaxmbU/uploaded-1777755330728-oe4g1r4z.jpg" },
-              { id: "p3", name: "شامبو إكليل الجبل + شامبو بذور العنب للقشرة ", price: "LD 139", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Cx9nk9sCSbinRaOXqM7TyaxmbU/uploaded-1777759406700-crigm2z4.jpg" }
+              { id: "p1", name: "شامبو وسيروم الكافيين", price: "مستحضر غني بالكافيين، الكولاجين، والبيوتين. يساعد في معالجة تساقط الشعر، تغذية البصيلات من الجذور، ومنح شعرك لمعاناً طبيعياً. مناسب للاستخدام اليومي وخالٍ من السلفات والبارابين.", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Cx9nk9sCSbinRaOXqM7TyaxmbU/uploaded-1777741219362-l83s1kf5.jpg" },
+              { id: "p2", name: "مجموعة العناية المتكاملة", price: "تشكيلة شاملة من شامبو الكافيين، شامبو بذور العنب، وشامبو إكليل الجبل. حل متكامل لتغذية الشعر وتقوية بنيته من الجذور، مع تركيبة آمنة تناسب مختلف أنواع الشعر.", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Cx9nk9sCSbinRaOXqM7TyaxmbU/uploaded-1777755330728-oe4g1r4z.jpg" },
+              { id: "p3", name: "شامبو إكليل الجبل وبذور العنب", price: "LD 139", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Cx9nk9sCSbinRaOXqM7TyaxmbU/uploaded-1777759406700-crigm2z4.jpg" }
             ]}
-            title="من هنا يبدا اهتمامك لشعرك  "
-            description="نهتم بشعرك وصحته لان هذا واجبنا اتجاهكم  "
+            title="من هنا يبدأ اهتمامك بشعرك"
+            description="نهتم بصحة شعرك لأن ذلك جوهر اهتمامنا بكم."
           />
         </div>
 
@@ -87,11 +86,11 @@ export default function LandingPage() {
             textboxLayout="default"
             useInvertedBackground={false}
             features={[
-              { title: "فوائد شامبو وسيروم الكافيين  ", description: "غني بالكافيين والكولاجين والبيوتين لمعالجة تساقط الشعر وتغذية الشعر وتعزيز بصيلاتها من الجذور وجعلها أقوى وألمع\nمناسب للاستخدام اليومي لشعر ناعم وطبيعي \nمستحضر لجميع أنواع الشعر خالي من السلفات و البرابين ", media: { imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Cx9nk9sCSbinRaOXqM7TyaxmbU/uploaded-1777759857284-tmxsyp86.jpg", imageAlt: "Foundation bottles" }, items: [{ icon: Leaf, text: "لعناية صحية يومية لشعرك " }], reverse: false },
-              { title: "( الشامبو إكليل الجبل) ", description: "( الشامبو إكليل الجبل )\nيقوي الجذور: يحتوي على الكيراتين لدعم نمو الشعر الصحي.\nيضيف لمعان: غني بالكولاجين للمساعدة على استعادة النعومة واللمعان.\nوبشكل عام، بما أنه شامبو بإكليل الجبل مع كيراتين وكولاجين، فوائده\nتقليل تساقط الشعر.\nتحفيز الدورة الدموية في فروة الرأس.\nتقوية الشعرة من الداخل.\nترطيب وتحسين مظهر الشعر الباهت أو التالف\nخالي من السلفات و البرابين", media: { imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Cx9nk9sCSbinRaOXqM7TyaxmbU/uploaded-1777759806647-okfzfs73.jpg", imageAlt: "Science lab" }, items: [{ icon: FlaskConical, text: "لعناية اكثر ولمعان اقوة " }], reverse: true }
+              { title: "شامبو وسيروم الكافيين", description: "تركيبة غنية بالكافيين والكولاجين والبيوتين لمعالجة تساقط الشعر وتعزيز صحة البصيلات. يمنحك شعراً أقوى وأكثر لمعاناً بشكل طبيعي، ومناسب للاستخدام اليومي.", media: { imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Cx9nk9sCSbinRaOXqM7TyaxmbU/uploaded-1777759857284-tmxsyp86.jpg", imageAlt: "Senerita caffeine care" }, items: [{ icon: Leaf, text: "عناية صحية يومية" }], reverse: false },
+              { title: "شامبو إكليل الجبل", description: "يحتوي على الكيراتين والكولاجين لتحفيز نمو الشعر، تقوية الجذور، وتحسين مرونة الشعرة من الداخل. مثالي لترطيب الشعر التالف واستعادة نعومته.", media: { imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Cx9nk9sCSbinRaOXqM7TyaxmbU/uploaded-1777759806647-okfzfs73.jpg", imageAlt: "Senerita rosemary care" }, items: [{ icon: FlaskConical, text: "لمعان فائق وقوة إضافية" }], reverse: true }
             ]}
-            title="صحة الشعر من صحتك وأناقتك "
-            description="منتجات غنية بالمستخلصات الطبيعية لفروة شعر ناعمة وحيوية "
+            title="صحة الشعر تبدأ من عنايتك"
+            description="منتجات طبيعية تمنحك فروة شعر صحية وناعمة."
           />
         </div>
 
@@ -102,11 +101,11 @@ export default function LandingPage() {
             gridVariant="three-columns-all-equal-width"
             useInvertedBackground={false}
             testimonials={[
-              { id: "t1", name: "FATIMA ", role: "sdfghj", company: "kjhgf ", rating: 5, imageSrc: "http://img.b2bpic.net/free-photo/close-up-portrait-coquettish-brunette-woman-laughing-smiling-looking-down-flirty-standing-ove_1258-87409.jpg?_wi=2" },
-              { id: "t2", name: "Fatima ", role: "sdfghjk ", company: "", rating: 5, imageSrc: "http://img.b2bpic.net/free-photo/woman-corrects-her-hairstyle-looks-aside_8353-10717.jpg" }
+              { id: "t1", name: "Fatima", role: "Hair Care Enthusiast", company: "Senerita Fan", rating: 5, imageSrc: "http://img.b2bpic.net/free-photo/close-up-portrait-coquettish-brunette-woman-laughing-smiling-looking-down-flirty-standing-ove_1258-87409.jpg?_wi=2" },
+              { id: "t2", name: "Fatima", role: "Professional User", company: "Happy Customer", rating: 5, imageSrc: "http://img.b2bpic.net/free-photo/woman-corrects-her-hairstyle-looks-aside_8353-10717.jpg" }
             ]}
-            title="dddddd "
-            description="asdfghjkl "
+            title="آراء عملائنا"
+            description="ما يقوله محبو منتجاتنا عن تجربتهم معنا."
           />
         </div>
 
@@ -114,11 +113,11 @@ export default function LandingPage() {
           <FaqSplitText
             useInvertedBackground={true}
             faqs={[
-              { id: "q1", title: "للمتابعة عبر الواتس آب  ", content: "https://wa.me/c/218946268415 " },
-              { id: "q2", title: "للمتابعة عبر الإنستقرام ", content: "https://www.instagram.com/senerita_libya " },
-              { id: "q3", title: "للمتابعة عبر الفيس بوك ", content: "https://www.facebook.com/share/1EDmHvo3YN/?mibextid=wwXIfr." }
+              { id: "q1", title: "للمتابعة عبر الواتس آب", content: "https://wa.me/c/218946268415" },
+              { id: "q2", title: "للمتابعة عبر الإنستقرام", content: "https://www.instagram.com/senerita_libya" },
+              { id: "q3", title: "للمتابعة عبر الفيس بوك", content: "https://www.facebook.com/share/1EDmHvo3YN/?mibextid=wwXIfr" }
             ]}
-            sideTitle="لطلبات والإستفسارات يرجاء متابعتنا"
+            sideTitle="لأي استفسار يرجى التواصل معنا"
             faqsAnimation="slide-up"
           />
         </div>
@@ -127,14 +126,14 @@ export default function LandingPage() {
           <ContactText
             useInvertedBackground={false}
             background={{ variant: "rotated-rays-static" }}
-            text="Ready to experience Senerita? Get in touch with our team today."
+            text="هل أنت مستعدة لتجربة سينيـريتا؟ تواصل معنا اليوم."
           />
         </div>
 
         <div id="footer" data-section="footer">
           <FooterCard
             logoText="Senerita Libya"
-            copyrightText="© 2025 Senerita Libya | All rights reserved."
+            copyrightText="© 2025 Senerita Libya | جميع الحقوق محفوظة."
           />
         </div>
       </ReactLenis>
